438907ab6e923bc6531c344ffb4f35f4cd3d58dd
djirou <arsen.djirou6@gmail.com>
2020-04-02 15:32:01 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      Hello from our recipes App
+    </div>
+  );
+}
+
+export default App;
