8fa1a94d18a0437de30d2e5443f0546a2167cbb4
djirou <arsen.djirou6@gmail.com>
2020-04-03 15:03:06 +0200

    single recipe page

--- src/data/tempList.js
+++ src/data/tempList.js
@@ -1,27 +1,27 @@
 export const recipeData = [
   {
     publisher: "Closet Cooking",
-    title: "Jalapeno Popper Grilled Cheese Sandwich",
+    title: "ancho chile powder",
     source_url:
       "http://www.closetcooking.com/2011/04/jalapeno-popper-grilled-cheese-sandwich.html",
-    recipe_id: "35382",
+    recipe_id: "1022009",
     image_url:
-      "https://www.allrecipes.com/recipe/214020/shakshuka-middle-eastern-breakfast-dish/?internalSource=rotd&referringId=78&referringContentType=Recipe%20Hub"
+      "https://spoonacular.com/cdn/ingredients_100x100/chili-powder.jpg"
   },
   {
     publisher: "The Pioneer Woman",
-    title: "Perfect Iced Coffee",
+    title: "beef tenderloin",
     source_url:
       "http://thepioneerwoman.com/cooking/2011/06/perfect-iced-coffee/",
-    recipe_id: "47024",
-    image_url: "http://static.food2fork.com/icedcoffee5766.jpg"
+    recipe_id: "1002030",
+    image_url: "https://spoonacular.com/cdn/ingredients_100x100/pepper.jpg"
   },
   {
     publisher: "The Pioneer Woman",
-    title: "Crash Hot Potatoes",
+    title: "coarse kosher salt",
     source_url:
       "http://thepioneerwoman.com/cooking/2008/06/crash-hot-potatoes/",
-    recipe_id: "47319",
-    image_url: "http://static.food2fork.com/CrashHotPotatoes5736.jpg"
+    recipe_id: "1082047",
+    image_url: "https://spoonacular.com/cdn/ingredients_100x100/salt.jpg"
   }
 ];
